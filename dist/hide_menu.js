915aad34f7209219034500ac481084d278de3799
c-128 <noemail@noemail.org>
2024-03-03 20:07:42 +0100

    updates hide_menu.js

--- dist/hide_menu.js
+++ dist/hide_menu.js
@@ -1,1 +1,1 @@
-(()=>{var e=document.querySelector("#topBar");e.remove();})();
+(()=>{var e={menu:document.querySelector("#topBar"),game:document.querySelector("#game")};e.remove();game.style.top="0";})();

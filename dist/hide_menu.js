8c193a5abd7121cab10acdebf659ec8bc87add44
c-128 <noemail@noemail.org>
2024-03-03 20:06:22 +0100

    adds hide_menu.js

---
+++ dist/hide_menu.js
@@ -0,0 +1,1 @@
+(()=>{var e=document.querySelector("#topBar");e.remove();})();
---
+++ src/bookmarklets/hide_menu.js
@@ -0,0 +1,7 @@
+const el = {
+  menu: document.querySelector("#topBar"),
+  game: document.querySelector("#game"),
+};
+
+el.remove();
+game.style.top = "0";
